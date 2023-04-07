d8480ba5cdb578967516fc725cdfee4d68b5c2dd
mjpcomp <7637082+mjpcomp@users.noreply.github.com>
2023-04-07 12:30:37 -0700

    Update SourceTenant.ps1

--- SourceTenant.ps1
+++ SourceTenant.ps1
@@ -5,10 +5,11 @@
 # Connect-ExchangeOnline -UserPrincipalName <AdministrativeUserAccount> -UseRPSSession
 # This will export users from the source tenant with the CustomAttribute1 = "Cross-Tenant-Dest-NAME"
 # Make sure the CustomAttribute1 is set within EXCHANGE, under user Mailbox setting -> Others -> Custom Attributes
+# The CustomAttribute should NOT have any spaces
 $crossTenantDest = "NAME"
 # Set the crossTenantDest to the appropriate TARGET TENANT
-$outFileUsers = "$home\desktop\UsersToMigrate.txt"
-$outFileUsersXML = "$home\desktop\UsersToMigrate.xml"
+$outFileUsers = "$home\desktop\UsersToMigrate-$crossTenantDest.txt"
+$outFileUsersXML = "$home\desktop\UsersToMigrate-$crossTenantDest.xml"
 Get-Mailbox -Filter "CustomAttribute1 -like 'Cross-Tenant-Dest-$crossTenantDest'" -ResultSize Unlimited | Select-Object -ExpandProperty  Alias | Out-File $outFileUsers
 $mailboxes = Get-Content $outFileUsers
 $mailboxes | ForEach-Object {Get-Mailbox $_} | Select-Object PrimarySMTPAddress,Alias,SamAccountName,FirstName,LastName,DisplayName,Name,ExchangeGuid,ArchiveGuid,LegacyExchangeDn,EmailAddresses | Export-Clixml $outFileUsersXML
